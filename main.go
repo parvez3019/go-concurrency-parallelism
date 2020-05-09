e4ec550c049ed3d15118d105c6a515fe3c75657a
Parvez Hassan <pha3019@gmail.com>
2020-05-09 17:36:28 +0530

    Add basic of go and routines with wg

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("helloWorld")
+	// a := make([]int, 10)
+	// variable initialization
+	// slices
+	// make
+	// for loops
+	// function calling -> function can return multiple values
+	// GOMAXPROCS
+	// error
+	// main -> go routine -> simple will be to add time.second
+	// wait groups
+
+	//listOfSites := []string{
+	//	"www.google.com",
+	//	"www.facebook.com",
+	//	"www.amazon.com",
+	//	"www.stackoverflow.com",
+	//	"www.linkedin.com",
+	//	"www.github.com",
+	//	"jigsaw.thoughtworks.net",
+	//}
+
+	runtime.GOMAXPROCS(1)
+	var wg sync.WaitGroup
+	start := time.Now()
+
+	a := []int{5, 10, 25, 35, 44, 45, 1}
+	for _, site := range a {
+		wg.Add(1)
+		//go get(&wg, site)
+		go fib(&wg, site)
+	}
+	wg.Wait()
+
+	fmt.Printf("time elapsed %v \n", time.Since(start))
+	fmt.Println("This is an awesome geek night!")
+	fmt.Println(runtime.NumCPU())
+}
+
+func fib(s *sync.WaitGroup, site int) {
+	fmt.Println(getfib(site))
+	s.Done()
+}
+
+func getfib(site int) int {
+	if site < 2 {
+		return site
+	}
+	return getfib(site-1) + getfib(site-2)
+}
+
+func get(wg *sync.WaitGroup, site string) {
+	res, _ := http.Get("https://" + site)
+	fmt.Printf("%s response -> %d \n", site, res.StatusCode)
+	wg.Done()
 }
