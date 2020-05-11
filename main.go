8eb3b334dada043fe8d82db1ca0bf9373b8f8ef4
Parvez Hassan <pha3019@gmail.com>
2020-05-12 03:43:37 +0530

    remove unused files

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	RunRestaurant()
-	// a := make([]int, 10)
-	// variable initialization
-	// slices
-	// make
 	// for loops
 	// function calling -> function can return multiple values
 	// GOMAXPROCS
@@ -15,49 +10,4 @@ func main() {
 
 	// Concurreny example - website response
 	// Parallelism - calculate fibonnaci sum of numbers
-	//
-	//
-	//listOfSites := []string{
-	//	"jigsaw.thoughtworks.net",
-	//	"www.google.com",
-	//	"www.facebook.com",
-	//	"www.amazon.com",
-	//	"www.stackoverflow.com",
-	//	"www.linkedin.com",
-	//	"www.github.com",
-	//}
-	//
-	//runtime.GOMAXPROCS(1)
-	//var wg sync.WaitGroup
-	//start := time.Now()
-	//
-	////a := []int{5, 10, 25, 35, 44, 45, 1}
-	//for _, site := range listOfSites {
-	//	wg.Add(1)
-	//	go get(&wg, site)
-	//	//go fib(&wg, site)
-	//}
-	//wg.Wait()
-	//
-	//fmt.Printf("time elapsed %v \n", time.Since(start))
-	//fmt.Println("This is an awesome geek night!")
-	//fmt.Println(runtime.NumCPU())
 }
-//
-//func fib(s *sync.WaitGroup, site int) {
-//	fmt.Println(getfib(site))
-//	s.Done()
-//}
-//
-//func getfib(site int) int {
-//	if site < 2 {
-//		return site
-//	}
-//	return getfib(site-1) + getfib(site-2)
-//}
-//
-//func get(wg *sync.WaitGroup, site string) {
-//	res, _ := http.Get("https://" + site)
-//	fmt.Printf("%s response -> %d \n", site, res.StatusCode)
-//	wg.Done()
-//}
