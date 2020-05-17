16cc98aa19b4aa3427e921945fedb3ff57546cf5
Sayan <shayan1337@gmail.com>
2020-05-17 21:03:26 +0530

    modify waitgroup code

--- present/waitGroup.go
+++ present/waitGroup.go
@@ -4,19 +4,27 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 )
 // START OMIT
 func main() {
-	sites := []string {"www.google.com", "www.facebook.com", "www.thoughtworks.com"}
+	sites := []string {
+		"google.com",
+		"facebook.com",
+		"thoughtworks.com",
+		"github.com",
+	}
 
 	var wg sync.WaitGroup
+
+	start := time.Now()
 
 	for _, site := range sites {
 		wg.Add(1)
 		go printResponse(site, &wg)
 	}
 	wg.Wait()
-	fmt.Println("exit main")
+	fmt.Println("Total time taken for the responses: ", time.Since(start))
 }
 
 func printResponse(site string, wg *sync.WaitGroup) {
