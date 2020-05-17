15f380f5845fc09c118b6ae9340a97a8abfaf86f
Sayan <shayan1337@gmail.com>
2020-05-18 01:25:32 +0530

    modify pingpong code

--- present/pingpong.go
+++ present/pingpong.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"time"
 )
 
 // START OMIT
 func main() {
+	runtime.GOMAXPROCS(1)
+
 	var Ball int
 	table := make(chan int)
 
-	go player(table)
-	go player(table)
+	go player(table, "Player 1")
+	go player(table, "Player 2")
 
 	table <- Ball
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Second)
 	<-table
 }
 
-func player(table chan int) {
+func player(table chan int, playerName string) {
 	for {
 		ball := <-table
+		fmt.Printf("%s taking ball from %d table\n", playerName, ball)
 		ball++
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(1 * time.Second)
+		fmt.Printf("%s putting ball %d on table\n", playerName, ball)
 		table <- ball
 	}
 }
